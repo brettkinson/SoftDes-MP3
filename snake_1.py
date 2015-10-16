e218059c34400f23fa8eea83fe3b9ed9ef69ccc0
brettkinson <bgatkinson@comcast.net>
2015-10-16 14:31:31 -0400

    update

--- snake_1.py
+++ snake_1.py
@@ -10,21 +10,28 @@ First itereation of Snake for Mini Project 3
 """
 
 import pygame
+import random
+import math
+import time
 
 pygame.init()
 
-screen = pygame.display.set_mode((640,480)) # set window size
+screen_size = (640,480)
+screen = pygame.display.set_mode(screen_size) # set window size
+
 background = pygame.Surface(screen.get_size()) # generate empty pygame surface
 background.fill((255,255,255)) # background color fill
 background = background.convert() # improve blitting
 
 screen.blit(background, (0, 0))
 
-for event in pygame.event.get():
-    if event.type == pygame.QUIT: 
-        mainloop = False # window closed
-    elif event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_ESCAPE:
-            mainloop = False # ESC pressed event
+running = True
+while running:
+	for event in pygame.event.get():
+	    if event.type == pygame.QUIT: 
+	        running = False # window closed
+	    elif event.type == pygame.KEYDOWN:
+	        if event.key == pygame.K_ESCAPE:
+	            running = False # ESC pressed event
 
 pygame.quit()
